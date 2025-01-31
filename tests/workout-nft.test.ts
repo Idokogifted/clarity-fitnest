ef4a7ef06b11f8902408f005ce1b947c3bf94be1
Idokogifted <IdokoGift@icloud.com>
2025-01-31 21:26:18 +0100

    test: Add WorkoutNFT tests

---
+++ tests/workout-nft.test.ts
@@ -0,0 +1,18 @@
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure that NFT minting works",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall("workout-nft", "mint", 
+        [types.principal(deployer.address), types.uint(1)], deployer.address)
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    block.receipts[0].result.expectOk().expectUint(1);
+  },
+});
